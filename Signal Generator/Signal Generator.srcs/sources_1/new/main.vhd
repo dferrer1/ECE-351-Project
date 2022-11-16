9a2b738c8df853ca37c1cb000693ce55fa2c0342
achin2 <achin2@vols.utk.edu>
2022-11-16 14:47:56 -0500

    some amplitude scaling (but it doesnt work)

--- Signal Generator/Signal Generator.srcs/sources_1/new/main.vhd
+++ Signal Generator/Signal Generator.srcs/sources_1/new/main.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity main is
     Port ( wave_type : in std_logic_vector (2 downto 0);
-           amplitude : in std_logic_vector (3 downto 0);
+           amplitude : in std_logic_vector (2 downto 0);
            frequency_up: in std_logic;
            frequency_down : in std_logic;
            clk : in std_logic ;
@@ -63,8 +63,19 @@ signal LUT_out : std_logic_vector (15 downto 0);
 signal offset : std_logic_vector (19 downto 0);
 signal init : std_logic ;
 signal test_large_out : std_logic_vector (19 downto 0);
+signal numerator : std_logic_vector (3 downto 0);
+signal mag_placeholder : std_logic_vector (40 downto 0);
 begin
 phase_accum : Phase_accumulator_for_diego_ref port map(clk => clk, init => init,offset => offset, LUT_address => LUT_address , large_out => test_large_out);
 LUT : blk_mem_gen_0 port map(clka => clk, wea => "0", addra => LUT_in , dina => "0",douta => LUT_out);
 
+--try and do some arithetic to figure out the amplitude.
+--This always needs to be a power of 2 so we are going to say the lowest value is hardwired to 0
+--Use three switches to be bits 3-1.  
+--the offset switches should be able to be directly what you shift.
+--proof of concept : desired amplitude = 1/2.  Numerator of 8, denominator of 16.
+-- So, to multiply by 8, shift to the left by 3
+-- Then, to divide back out by the denominator, shift it back right by 4. 
+mag_placeholder <= std_logic_vector(shift_left(unsigned(LUT_out),to_integer(unsigned(amplitude))));
+DAC_out <= std_logic_vector (shift_right (unsigned(mag_placeholder),4));
 end Behavioral;
